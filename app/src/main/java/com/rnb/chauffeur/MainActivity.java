c0f1e58dc0755ea8be7ad8783e6283cb14af4f0d
RetroPugalicious <nws6886@gmail.com>
2022-03-05 17:10:10 -0500

    Solo vs Party Join

--- app/src/main/java/com/rnb/chauffeur/MainActivity.java
+++ app/src/main/java/com/rnb/chauffeur/MainActivity.java
@@ -1,10 +1,17 @@
 package com.rnb.chauffeur;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputType;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -31,7 +38,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(new Intent(MainActivity.this, RoomActivity.class));
                 break;
             case R.id.party:
-                startActivity(new Intent(MainActivity.this, RoomActivity.class));
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage("Would you like to join an existing party or create your own?")
+                        .setPositiveButton("Create", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                startActivity(new Intent(MainActivity.this, RoomActivity.class));
+                            }
+                        })
+                        .setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // FIRE ZE MISSILES!
+                            }
+                        })
+                        .setNegativeButton("Join", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                startActivity(new Intent(MainActivity.this, RoomJoinActivity.class));
+                            }
+                        });
+                // Create the AlertDialog object and return it
+                builder.show();
                 break;
         }
     }
--- app/src/main/java/com/rnb/chauffeur/RoomActivity.java
+++ app/src/main/java/com/rnb/chauffeur/RoomActivity.java
@@ -1,6 +1,9 @@
 package com.rnb.chauffeur;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -14,6 +17,15 @@ public class RoomActivity extends AppCompatActivity {
         assert getSupportActionBar() != null;   //null check
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setTitle("ROOM");
+
+        replaceFragment(new MemberFragment());
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.framelayout, fragment);
+        fragmentTransaction.commit();
     }
 
     @Override
