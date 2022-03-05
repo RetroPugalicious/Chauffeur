aedc5de146bddeda26edffa1b86c46db3b7d7611
RetroPugalicious <nws6886@gmail.com>
2022-03-05 18:00:23 -0500

    First Half of UI Finalized

--- app/src/main/java/com/rnb/chauffeur/MainActivity.java
+++ app/src/main/java/com/rnb/chauffeur/MainActivity.java
@@ -3,13 +3,8 @@ package com.rnb.chauffeur;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.InputType;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -35,26 +30,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.solo:
-                startActivity(new Intent(MainActivity.this, RoomActivity.class));
+                Intent i = new Intent(MainActivity.this, RoomActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("role", "Leader");
+                i.putExtras(bundle);
+                startActivity(i);
                 break;
             case R.id.party:
                 AlertDialog.Builder builder = new AlertDialog.Builder(this);
                 builder.setMessage("Would you like to join an existing party or create your own?")
-                        .setPositiveButton("Create", new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                startActivity(new Intent(MainActivity.this, RoomActivity.class));
-                            }
+                        .setPositiveButton("Create", (dialog, id) -> {
+                            Intent i1 = new Intent(MainActivity.this, RoomActivity.class);
+                            Bundle bundle1 = new Bundle();
+                            bundle1.putString("role", "Leader");
+                            i1.putExtras(bundle1);
+                            startActivity(i1);
                         })
-                        .setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                // FIRE ZE MISSILES!
-                            }
+                        .setNeutralButton("Cancel", (dialog, id) -> {
+
                         })
-                        .setNegativeButton("Join", new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                startActivity(new Intent(MainActivity.this, RoomJoinActivity.class));
-                            }
-                        });
+                        .setNegativeButton("Join", (dialog, id) -> startActivity(new Intent(MainActivity.this, RoomJoinActivity.class)));
                 // Create the AlertDialog object and return it
                 builder.show();
                 break;
--- app/src/main/java/com/rnb/chauffeur/RoomActivity.java
+++ app/src/main/java/com/rnb/chauffeur/RoomActivity.java
@@ -6,19 +6,35 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class RoomActivity extends AppCompatActivity {
+
+    String role = "undetermined";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room);
 
+        TextView errorText = findViewById(R.id.error);
+        errorText.setVisibility(View.INVISIBLE);
+
         assert getSupportActionBar() != null;   //null check
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setTitle("ROOM");
 
-        replaceFragment(new MemberFragment());
+        Bundle bundle = getIntent().getExtras();
+        if(bundle != null)
+            role = bundle.getString("role");
+
+        if(role.equals("Member"))
+            replaceFragment(new MemberFragment());
+        else if (role.equals("Leader"))
+            replaceFragment(new LeaderFragment());
+        else
+            errorText.setVisibility(View.VISIBLE);
     }
 
     private void replaceFragment(Fragment fragment) {
